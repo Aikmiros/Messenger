017baa194f2cd07496a3ac8a2dec251155eddef3
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-02-23 18:50:26 +0200

    add info about team

--- Program.cs
+++ Program.cs
@@ -3,8 +3,15 @@
 namespace Messenger {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Messenger");
+            Console.WriteLine("Version: 1");
+            Console.WriteLine("Authors: Grybenko Yegor, Sukhanova Maria, Trembach Anastasia");
+            Console.WriteLine("Group: IP-93");
+            Console.WriteLine("Brigade: 3");
+            Console.WriteLine("");
+            Console.WriteLine("Modeling start");
+            Console.WriteLine("Modeling end");
+            Console.ReadKey();
         }
     }
 }
