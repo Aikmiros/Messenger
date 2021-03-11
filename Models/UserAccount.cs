3a1c213e9f6f00c90c78bd3ea0fdc9f878c82670
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-03-11 19:31:43 +0200

    add constructors for UserAccount

--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -5,23 +5,52 @@ using System.Text;
 namespace Messenger {
     public class UserAccount {
 
-        public static UserAccount[] users;
-        public static int _nextUserId;
+        private static UserAccount[] users = new UserAccount[100];
+        private static int _nextUserId = 0;
 
-        public int id;
-        public string username;
-        public string password;
-        public string status;
-        public int[] chatRooms;
+        private int id;
+        private string username;
+        private string password;
+        private string status;
+        private int[] chatRooms;
+        
+        public UserAccount() {
+            this.id = UserAccount._nextUserId++;
+            this.username = "User" + this.id;
+            this.password = "User" + this.id;
+            this.status = "";
+            this.chatRooms = new int[100];
+            UserAccount.users[this.id] = this;
+            Console.WriteLine("UserAccount created constructor default");
+        }
 
-        public static void createUser(string username, string password) { }
+        public UserAccount(string username, string password) {
+            this.username = username;
+            this.password = password;
+            this.status = "";
+            this.id = UserAccount._nextUserId++;
+            this.chatRooms = new int[100];
+            UserAccount.users[this.id] = this;
+            Console.WriteLine("UserAccount created constructor inizialization");
+        }
+
+        public UserAccount(UserAccount oldUser) {
+            this.username = oldUser.username;
+            this.password = oldUser.password;
+            this.status = oldUser.status;
+            this.id = oldUser.id;
+            this.chatRooms = oldUser.chatRooms;
+            UserAccount.users[this.id] = this;
+            Console.WriteLine("UserAccount created constructor copy");
+        }
+
         public static void login(string username, string password) { }
 
-        public void deleteUser() { }
-        public void showChatRooms() { }
-        public void showUserInfo() { }
-        public void changePassword(string newPassword) { }
-        public void changeUsername(string newUsername) { }
-        public void changeStatus(string newStatus) { }
+        private void deleteUser() { }
+        private void showChatRooms() { }
+        private void showUserInfo() { }
+        private void changePassword(string newPassword) { }
+        private void changeUsername(string newUsername) { }
+        private void changeStatus(string newStatus) { }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Messenger {
             Console.WriteLine("Brigade: 3");
             Console.WriteLine("");
             Console.WriteLine("Modeling start");
+
+            UserAccount user1 = new UserAccount();
+            UserAccount user2 = new UserAccount("Anastasia", "123456");
+            UserAccount user3 = new UserAccount(user2);
+
             Console.WriteLine("Modeling end");
             Console.ReadKey();
         }
