68397eb5d46969a400b145b0fa27b2ba23a4da19
YAGoOaR <yegorgribenko68@gmail.com>
2021-04-07 21:13:45 +0300

    Add more virtual methods

--- Models/Chat.cs
+++ Models/Chat.cs
@@ -12,21 +12,20 @@ namespace Messenger
         protected List<int> participants;
         protected List<Message> messages;
         protected int id;
+        protected bool opened;
 
         public int Id { get { return id; } }
+        public bool Opened { get { return opened; } }
 
-        public List<int> Participants
-        {
+        public List<int> Participants {
             get { return participants; }
         }
 
-        public List<Message> Messages
-        {
+        public List<Message> Messages {
             get { return messages; }
         }
 
-        public Chat()
-        {
+        public Chat() {
             id = _nextRoomId++;
             chatRooms[id] = this;
             messages = new List<Message>();
@@ -34,40 +33,66 @@ namespace Messenger
             messages.Add(new Message());
         }
 
-        public Chat(int userId, string name)
-        {
+        public Chat(int userId, string name) {
             id = _nextRoomId++;
             messages = new List<Message>();
             participants = new List<int>();
             chatRooms[id] = this;
         }
 
-        public Chat(Chat from)
-        {
+        public Chat(Chat from) {
             id = _nextRoomId++;
             participants = from.Participants.Count == 0 ? new List<int>() : new List<int>(from.Participants);
             messages = new List<Message>();
             chatRooms[id] = this;
         }
 
-        public static void deleteRoom(int id)
-        {
+        public static void deleteRoom(int id) {
             chatRooms[id] = null;
         }
 
+        public static Chat FindChat(int id) {
+            if (id >= 0 && id < _nextRoomId){
+                return chatRooms[id];
+            }
+            else return null;
+        }
+
+        public void joinChat(UserAccount user) {
+            if (opened) {
+                participants.Add(user.Id);
+            }
+        }
+
+        //Следующие методы при надобности перезаписвыаем
+
+        //Первый аргумент - пользователь, который хочет добавить другого пользователя
+        //В методе проверяем, есть ли у него право это сделать а потом добавляем пользователя если права есть
+        //(GroupChat, Channel - добавлять может только админ, PersonalMessages  - никто)
+        public virtual void addParticipant(UserAccount user, int userId) {}
+        public virtual void deleteParticipant(UserAccount user, int userId) {}
+
+        //Управление доступом к чату. Если чат открыт, в него можно входить с помощью joinChat()
+        //Открывает чат в GroupChat и Channel админ, в PersonalMessages - никто
+        public virtual void openChat(UserAccount user, bool open) { }
+
         //Метод, в который передаём пользователя, который хочет отправить сообщение и само сообщение,
-        //проверяем есть ли у пользователя разрешение его отправить(например, в канале только автор может это делать),
-        //создаем сообщение и возвращаем положительный/отрицательный результат
-        public abstract bool sendMessage(UserAccount user, string messageBody);
+        //проверяем есть ли у пользователя разрешение его отправить(например, в канале только автор может это делать), и создаем сообщение
+        public virtual void sendMessage(UserAccount user, string messageBody) {
+            messages.Add(new Message(user.Id, messageBody));
+        }
 
         //То же самое с удалением
-        public abstract bool removeMessage(UserAccount user, int messageId);
+        public virtual void removeMessage(UserAccount user, int messageId) {
+            messages.RemoveAt(messageId);
+        }
 
         //Тоже проверяем, кто хочет очистить историю(GroupChat, channel - админ, PersonalMessages - не проверяем)
-        public abstract void clearHistory(UserAccount user);
+        public virtual void clearHistory(UserAccount user) {
+            messages.Clear();
+        }
 
-        public void showHistory()
-        {
+        public void showHistory() {
             messages.ForEach(msg => msg.show());
         }
 
