a0b2d4e659b69deed2810ac8543a3c5399049431
YAGoOaR <yegorgribenko68@gmail.com>
2021-04-07 19:59:00 +0300

    Remove stubs

--- Models/GroupChat.cs
+++ Models/GroupChat.cs
@@ -5,22 +5,6 @@ using System.Text;
 namespace Messenger {
     public class GroupChat : Chat
     {
-        public GroupChat()
-        {
-        }
 
-        public GroupChat(int userId, string name) : base(userId, name)
-        {
-        }
-
-        internal void addParticipant(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void deleteParticipant(int id)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
