5a1d22b5c5d3b778eadea484b2315aa9bea4d2ef
LovelyDrug <suhanovamasha21@gmail.com>
2021-04-08 12:23:08 +0300

    Add PersonalMsg and Channel

---
+++ Models/Channel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messenger
+{
+    public class Channel : GroupChat
+    {
+        public override bool sendMessage(UserAccount user, string messageBody)
+        {
+            if (user.Id == admin)
+            {
+                IMessage message = new TextMessage(user.Id, messageBody);
+                messages.Add(message);
+                return true;
+            }
+            else return false;
+        }
+        public override bool removeMessage(UserAccount user, int messageId)
+        {
+            if (user.Id == admin)
+            {
+                messages.RemoveAt(messageId);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Models/PersonalMessages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messenger.Models
+{
+    class PersonalMessages : Chat
+    {
+        public override void openChat(UserAccount user, bool open)
+        {
+            opened = false;
+        }
+        public override bool sendMessage(UserAccount user, string messageBody)
+        {
+            IMessage message = new TextMessage(user.Id, messageBody);
+            messages.Add(message);
+            return true;
+        }
+        public override bool removeMessage(UserAccount user, int messageId)
+        {
+            if (user.Id == messages[messageId].AuthorId)
+            {
+                messages.RemoveAt(messageId);
+                return true;
+            }
+            else return false;
+        }
+    }
+}
